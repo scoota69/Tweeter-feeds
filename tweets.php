dd7e044b2bf4086d8ba56863db2a18bf8f3009d3
student <student@105-8-4-II1092.local>
2012-06-13 11:17:05 -0400

    6th (making some more changes

--- tweets.php
+++ tweets.php
@@ -1,22 +1,38 @@
 <?php
 
     $json = file_get_contents("https://search.twitter.com/search.json?q=fitness&rpp=25&include_entities=true&result_type=mixed");
-
     $outputs = json_decode($json);
-        echo $outputs -> page." ";
-        echo $outputs -> query." ";
-        echo $outputs -> results."<br/>";
 ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<title>tweets</title>
+	<meta name="twitter" content="A template for tweets">
+	<!--Stylesheets-->
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="container">
 <?php
-$i = 1;
-while ($i <=25) {
-    echo $i++;
-?>
-    <div>
-        <img src=" <?php echo $outputs -> results[$i] -> profile_image_url;?>"/>
-        <p> <?php echo $outputs -> results[$i] -> from_user_name." "."@";
-              echo $outputs -> results[$i] -> from_user;?></p>
-         <p> <?php echo $outputs -> results[$i] -> text;
-            echo $outputs -> results[$i] -> created_at; ?> </p>
-    </div>        
+    
+  echo $outputs -> query." "."<br/>";
+        
+    $i = 1;
+    while ($i <=25) {
+        echo $i++;
+ ?>
+        <div class="tweet">
+            <img src=" <?php echo $outputs -> results[$i] -> profile_image_url;?>"/>
+            <p> <?php echo $outputs -> results[$i] -> from_user_name." "."@";
+                  echo $outputs -> results[$i] -> from_user;?></p>
+             <p> <?php echo $outputs -> results[$i] -> text;
+                echo $outputs -> results[$i] -> created_at; ?> </p>
+        </div>
+    </div>
 <?php } ?>
+</body>
+</html>
