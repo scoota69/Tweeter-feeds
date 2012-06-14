fbd896e3753d9e830f9ba852a39b1aeb6df3e45a
student <student@105-8-4-II1092.local>
2012-06-14 12:18:28 -0400

    3rd change(added reg express)

--- tweets.php
+++ tweets.php
@@ -13,6 +13,15 @@
 	<meta name="twitter" content="A template for tweets">
 	<!--Stylesheets-->
 	<link rel="stylesheet" href="css/style.css">
+	    
+	<!-- <script src="jquery-1.7.1.min.js"></script>
+	<script>
+	    $(function()
+{
+    var re = /(https?:\/\/(([-\w\.]+)+(:\d+)?(\/([\w/_\.]*(\?\S+)?)?)?))/ig;    
+    $('.text').html($('.text').html().replace(re, '<a href="$1" title="">$1</a>'));
+});
+	</script> -->
 </head>
 
 <body>
@@ -20,12 +29,31 @@
         <header id="header">
             <h1> Tweet results for <?php echo $outputs -> query." "."<br/>";?></h1>
         </header>     
-    <?php   
+    <?php
+    
+error_reporting(E_ALL & ~E_DEPRECATED);
+    
+function makeClickableLinks($text){
+
+        $text = html_entity_decode($text);
+        $text = " ".$text;
+        $text = ereg_replace('(((f|ht){1}tp://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)',
+                '<a href="\\1" target=_blank>\\1</a>', $text);
+        $text = ereg_replace('(((f|ht){1}tps://)[-a-zA-Z0-9@:%_\+.~#?&//=]+)',
+                '<a href="\\1" target=_blank>\\1</a>', $text);
+        $text = ereg_replace('([[:space:]()[{}])(www.[-a-zA-Z0-9@:%_\+.~#?&//=]+)',
+        '\\1<a href="http://\\2" target=_blank>\\2</a>', $text);
+        $text = ereg_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,3})',
+        '<a href="mailto:\\1" target=_blank>\\1</a>', $text);
+        return $text;
+}
+
         $i = 1;
         while ($i <=25) {
             $i++;
 	    
 	date_default_timezone_set('America/New_York');
+
     ?>
         <div class="tweet">
             <img class="img" src=" <?php echo $outputs -> results[$i] -> profile_image_url;?>"/>
@@ -33,7 +61,7 @@
                     <p class="user_name"><a href="https://twitter.com/#!/<?php echo "@".$outputs -> results[$i] -> from_user;?>">
                     <span class="user"><?php echo $outputs -> results[$i] -> from_user_name." ";?></span></a>
                      <span class="at_user"><?php echo "@".$outputs -> results[$i] -> from_user;?></span></p>
-                    <p class="text"> <?php echo $outputs -> results[$i] -> text;?> </p>	
+                    <p class="text"> <?php echo makeClickableLinks($outputs -> results[$i] -> text);?> </p>	
                 </div>
 		<div class="time">
                         <?php $timestamp = strtotime($outputs -> results[$i] -> created_at);
